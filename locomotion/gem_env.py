a151f5dcd487569972196b16d85d58cf1661c6a5
Yao-Xinchen <yao29@illinois.edu>
2025-03-18 15:33:06 -0500

    complete gem_env

--- locomotion/gem_env.py
+++ locomotion/gem_env.py
@@ -2,7 +2,8 @@ import torch
 import math
 import genesis as gs
 from genesis.ext.trimesh.path.packing import visualize
-from genesis.utils.geom import quat_to_xyz, transform_by_quat, inv_quat, transform_quat_by_quat
+from genesis.utils.geom import xyz_to_quat, quat_to_xyz, transform_by_quat, inv_quat, transform_quat_by_quat
+from sympy.physics.units import degree
 
 from gem_ackermann import GemAckermann
 
@@ -123,6 +124,11 @@ class GemEnv:
         self.reset_buf = torch.ones((self.num_envs,), device=self.device, dtype=gs.tc_int)
         self.episode_length_buf = torch.zeros((self.num_envs,), device=self.device, dtype=gs.tc_int)
         self.commands = torch.zeros((self.num_envs, self.num_commands), device=self.device, dtype=gs.tc_float)
+        self.crash_condition = torch.zeros((self.num_envs,), device=self.device, dtype=gs.tc_int)
+        self.rel_pos = torch.zeros((self.num_envs, 3), device=self.device, dtype=gs.tc_float)
+        self.last_rel_pos = torch.zeros_like(self.rel_pos)
+        self.base_euler = torch.zeros((self.num_envs, 3), device=self.device, dtype=gs.tc_float)
+        self.rel_yaw = torch.zeros((self.num_envs,), device=self.device, dtype=gs.tc_float)
 
         self.actions = torch.zeros((self.num_envs, self.num_actions), device=self.device, dtype=gs.tc_float)
         self.last_actions = torch.zeros_like(self.actions)
@@ -138,6 +144,21 @@ class GemEnv:
         self.steering = torch.zeros((self.num_envs,), device=self.device, dtype=gs.tc_float)
         self.speed = torch.zeros((self.num_envs,), device=self.device, dtype=gs.tc_float)
         self.outputs = torch.zeros((self.num_envs, len(joint_idx)), device=self.device, dtype=gs.tc_float)
+
+    def _resample_commands(self, envs_idx):
+        # command [x, y, orientation]
+        self.commands[envs_idx, 0] = gs_rand_float(*self.command_cfg["pos_x_range"], (envs_idx.sum(),), self.device)
+        self.commands[envs_idx, 1] = gs_rand_float(*self.command_cfg["pos_y_range"], (envs_idx.sum(),), self.device)
+        self.commands[envs_idx, 2] = gs_rand_float(-torch.pi, torch.pi, (envs_idx.sum(),), self.device)
+
+        if self.target is not None:
+            zeros = torch.zeros_like(self.commands[envs_idx, 0])
+            # position
+            pos = torch.stack([self.commands[envs_idx, 0], self.commands[envs_idx, 1], zeros], dim=1)
+            self.target.set_pos(pos, zero_velocity=True, envs_idx=envs_idx)
+            # orientation
+            euler = torch.stack([zeros, zeros, self.commands[envs_idx, 2]], dim=1)
+            self.target.set_quat(xyz_to_quat(euler, degrees=False), zero_velocity=True, envs_idx=envs_idx)
 
     def _at_target(self):
         gem_pos = self.base_pos
@@ -158,4 +179,131 @@ class GemEnv:
         # step the scene
         self.scene.step()
 
-        # TODO
+        # update buffers
+        self.episode_length_buf += 1
+        self.last_base_pos[:] = self.base_pos[:]
+        self.base_pos[:] = self.gem.get_pos()
+        self.rel_pos = self.commands[:, :2] - self.base_pos[:, :2]
+        self.last_rel_pos = self.commands[:, :2] - self.last_base_pos[:, :2]
+        self.base_quat[:] = self.gem.get_quat()
+        self.base_euler = quat_to_xyz(
+            transform_quat_by_quat(torch.ones_like(self.base_quat) * self.inv_base_init_quat, self.base_quat),
+            degrees=False
+        )
+        self.rel_yaw = (self.commands[:, 2] - self.base_euler[:, 2] + math.pi) % (2 * math.pi) - math.pi
+        inv_base_quat = inv_quat(self.base_quat)
+        self.base_lin_vel[:] = transform_by_quat(self.gem.get_vel(), inv_base_quat)
+        self.base_ang_vel[:] = transform_by_quat(self.gem.get_ang(), inv_base_quat)
+
+        # resample targets
+        envs_idx = self._at_target()
+        self._resample_commands(envs_idx)
+
+        self.crash_condition = (
+                (torch.abs(self.base_euler[:, 1]) > self.env_cfg["termination_if_pitch_greater_than"])
+                | (torch.abs(self.base_euler[:, 0]) > self.env_cfg["termination_if_roll_greater_than"])
+                | (torch.abs(self.rel_pos[:, 0]) > self.env_cfg["termination_if_x_greater_than"])
+                | (torch.abs(self.rel_pos[:, 1]) > self.env_cfg["termination_if_y_greater_than"])
+        )
+        self.reset_buf = (self.episode_length_buf > self.max_episode_length) | self.crash_condition
+
+        time_out_idx = (self.episode_length_buf > self.max_episode_length).nonzero(as_tuple=False).flatten()
+        self.extras["time_outs"] = torch.zeros_like(self.reset_buf, device=self.device, dtype=gs.tc_float)
+        self.extras["time_outs"][time_out_idx] = 1.0
+
+        self.reset_idx(self.reset_buf.nonzero(as_tuple=False).flatten())
+
+        # compute rewards
+        self.rew_buf[:] = 0.0
+        for name, reward_func in self.reward_functions.items():
+            rew = reward_func() * self.reward_scales[name]
+            self.rew_buf += rew
+            self.episode_sums[name] += rew
+
+        self.obs_buf = torch.cat(
+            [
+                self.rel_pos * self.obs_scales["rel_pos"],
+                self.last_rel_pos * self.obs_scales["rel_pos"],
+                self.rel_yaw.unsqueeze(1) * self.obs_scales["rel_yaw"],
+                self.base_lin_vel,
+                self.base_ang_vel,
+            ],
+            dim=-1,
+        )
+
+        self.last_actions[:] = self.actions[:]
+
+        return self.obs_buf, None, self.rew_buf, self.reset_buf, self.extras
+
+    def get_observations(self):
+        return self.obs_buf
+
+    def get_privileged_observations(self):
+        return None
+
+    def reset_idx(self, envs_idx):
+        if len(envs_idx) == 0:
+            return
+
+        # reset gem
+        self.base_pos[envs_idx] = self.base_init_pos
+        self.last_base_pos[envs_idx] = self.base_init_pos
+        self.rel_pos = self.commands - self.base_pos
+        self.last_rel_pos = self.commands - self.last_base_pos
+        self.base_quat[envs_idx] = self.base_init_quat.reshape(1, -1)
+        self.gem.set_pos(self.base_pos[envs_idx], zero_velocity=True, envs_idx=envs_idx)
+        self.gem.set_quat(self.base_quat[envs_idx], zero_velocity=True, envs_idx=envs_idx)
+        self.base_lin_vel[envs_idx] = 0
+        self.base_ang_vel[envs_idx] = 0
+        self.gem.zero_all_dofs_velocity(envs_idx)
+
+        # reset buffers
+        self.last_actions[envs_idx] = 0.0
+        self.actions[envs_idx] = 0.0  # Reset current actions too
+        self.episode_length_buf[envs_idx] = 0
+        self.reset_buf[envs_idx] = True
+        self.crash_condition[envs_idx] = 0  # Reset crash condition
+        self.rew_buf[envs_idx] = 0.0  # Reset reward buffer
+
+        # Reset control outputs
+        self.steering[envs_idx] = 0.0
+        self.speed[envs_idx] = 0.0
+        self.outputs[envs_idx] = 0.0
+
+        # Reset base euler and rel_yaw
+        self.base_euler[envs_idx, :] = 0.0
+        self.rel_yaw = (self.commands[:, 2] - self.base_euler[:, 2] + math.pi) % (2 * math.pi) - math.pi
+
+        # fill extras
+        self.extras["episode"] = {}
+        for key in self.episode_sums.keys():
+            self.extras["episode"]["rew_" + key] = (
+                    torch.mean(self.episode_sums[key][envs_idx]).item() / self.env_cfg["episode_length_s"]
+            )
+            self.episode_sums[key][envs_idx] = 0.0
+
+        self._resample_commands(envs_idx)
+
+    def reset(self):
+        self.reset_buf[:] = True
+        self.reset_idx(torch.arange(self.num_envs, device=self.device))
+        return self.obs_buf, None
+
+    # ------------ reward functions----------------
+    def _reward_target(self):
+        target_rew = torch.sum(torch.square(self.last_rel_pos), dim=1) - torch.sum(torch.square(self.rel_pos), dim=1)
+        return target_rew
+
+    def _reward_smooth(self):
+        smooth_rew = torch.sum(torch.square(self.actions - self.last_actions), dim=1)
+        return smooth_rew
+
+    def _reward_yaw(self):
+        yaw_rew = torch.exp(self.reward_cfg["yaw_lambda"] * torch.abs(self.rel_yaw))
+        return yaw_rew
+
+    def _reward_crash(self):
+        crash_rew = -self.crash_condition.float()
+        return crash_rew
+
+    # TODO
